7a5ed931ff88e274c0d4981e1f8a2fbb5197493a
limleonardoo <36912851+limleonardoo@users.noreply.github.com>
2018-03-23 21:28:52 +0700

    Coba Coding AI Movement

---
+++ Shuriken Sloth/Assets/Script/EnemyScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EnemyScript: MonoBehaviour
+{
+    public Transform Player;
+    public float Speed = 5;
+    
+    void Update()
+    {
+        Vector3 SelisihPosisi = Player.transform.position - this.transform.position;
+        if (Vector3.Distance(Player.position, transform.position)<10)
+        {
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(SelisihPosisi), 0.1f);
+            this.transform.Translate(0, 0, Speed * Time.deltaTime);
+
+            Debug.Log("Player Approaching");
+        
+        }
+    }
+}
