8f69db2869dee919037abafa5845efa9f2f47a10
limleonardoo <36912851+limleonardoo@users.noreply.github.com>
2018-03-23 19:01:27 +0700

    Tambahin Asset Tapi masih cacat ko

---
+++ Shuriken Sloth/Assets/Script/MainMenuScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour {
+    public GameObject CanvasOptions;
+    public GameObject CanvasMainMenu;
+    private void Start()
+    {
+        CanvasOptions.SetActive(false);
+        CanvasMainMenu.SetActive(true);
+        
+    }
+    public void PindahScene (string NamaScene)
+    {
+        SceneManager.LoadScene(NamaScene);
+    }
+    public void Options()
+    {
+        CanvasOptions.SetActive(true);
+        CanvasMainMenu.SetActive(false);
+    }
+    public void KeluarAja() {
+        Application.Quit();
+    }
+}
---
+++ Shuriken Sloth/Assets/Script/MasterKarakter.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MasterKarakter : MonoBehaviour
+{
+    public GameObject Pelurunya;
+    public GameObject PosisiPeluru;
+
+    public float speed;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Navigasi();
+        Tembak();
+    }
+
+    void Navigasi()
+    {
+        if (Input.GetKey(KeyCode.W))
+
+        {
+            transform.Translate(0, 0, speed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, 0, -speed * Time.deltaTime);
+        }
+
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(speed * Time.deltaTime, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(-speed * Time.deltaTime, 0, 0);
+        }
+    }
+
+    void Tembak()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(Pelurunya,PosisiPeluru.transform.position,PosisiPeluru.transform.rotation);
+        }
+    }
+}
+
