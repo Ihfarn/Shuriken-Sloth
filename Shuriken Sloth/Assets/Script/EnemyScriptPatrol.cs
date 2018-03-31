d89d480a61fb699cc8715966bb25d9f97549e0a9
limleonardoo <36912851+limleonardoo@users.noreply.github.com>
2018-03-31 13:12:17 +0700

    Nambahin asset di dalam unity

---
+++ Shuriken Sloth/Assets/Script/EnemyScriptPatrol.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class EnemyScriptPatrol : MonoBehaviour {
+    public float waktu;
+    public float speed;
+    public Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        StartCoroutine(wait(waktu));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rb.velocity = new Vector3(speed, 0f, 0f);
+        
+
+    }
+
+    IEnumerator wait(float waktu)
+    {
+        yield return new WaitForSeconds(waktu);
+        speed *= -1;
+        StartCoroutine(wait(waktu));
+    }
+}
